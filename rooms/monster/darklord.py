8102a5cc30c748a7d26291e56790d3cc71216cb8
Dmitriy Varenko <Dm.varenkov@gmail.com>
2016-09-02 18:47:11 +0300

    update Darklord and fix mimic

--- rooms/monster/darklord.py
+++ rooms/monster/darklord.py
@@ -1,80 +1,120 @@
 from constants import *
 
-FIGHT = 'За Шир!!!'
-ESCAPE = 'Попытаться убежать'
+ACTIVATED = 'activated'
 
-name = 'Темный лорд'
-hp = 1000  # Темный Лорд должен быть лютым парнем! А то что-то они там все перекаченные какие-то
-damage_range = ( 20, 25 )
+actions_state_0 = ['Уходим отсюда']  # darclord passive
+actions_state_1 = ['За Шир!!!', 'Сушим весла!!!']
+actions_state_2 = ['Моя прелесть! МОЯ!']
 
-coins = 0  # Темному Лорду не нужны деньги! Он берет все что захочет!
+name = 'Темный Лорд'
+hp = 1500
+damage_range = (50, 75)
 
-loot = [ 'ring' ]
+coins = 0
 
-
-def enter(user, reply):
-	reply(
-		'Кажется мы оказались не в том месте в не то время.\n'
-		'Очень надеюсь что {} не обратит на нас внимание.'.format(name)
-	)
-
-	if user.story_level < 25:
-		reply(
-			'Кажется он вас не заметил.\n'
-			'Вот и славненько, пойдем отсюда скорее, не будем мешать'
-		)
-
-		user.leave(reply)
-
-	else:
-
-		if not user.has_item('rigs'):
-			reply(
-				'Апчих!\n'
-				'Как тут пыльно!\n'
-				'ОЙ! По Моему нас заметили!'
-			)
-
-		else:
-			user.remove_item('rigs')  # У игрока должно быть только одно кольцо!
-			reply(
-				'«_Я чувстую его! Оно мое!_»\n'
-				'Мамочки! Мне страшно!\n'
-				'Верни ему то что он просит! НЕМЕДЛЕННО!'
-			)
-
-
-def dice(user, reply, result, subject=None):
-
-	if subject == ESCAPE:
-
-		if result > DICE_MIDDLE:
-			reply('Тебе удалось! Надеюсь мы больше не встретим этого мерзкого типа!')
-			user.leave(reply)
-
-		else:
-			reply('Не вышло!\nНу ни чего я буду за тебя болеть!')
-			# тут нужно запустить обычый бой
-
-
-def action(user, reply, text):
-	question = user.get_room_temp('question')
-
-	if text == FIGHT:
-		reply(
-			'Пойду ка я спрячусь вон за ту колонну! '
-			'А вы тут пока общайтесь!'
-		)
-		# тут нужно запустить обычый бой
-
-
-	elif text == ESCAPE:
-		reply(
-			'«_Апчих! Пора тут прибарться_»\n'
-			'Кидай кубики, это наш шанс!!!'
-		)
-		user.throw_dice(reply, ESCAPE)
+loot = ['ring']
 
 
 def get_actions(user):
-	return [FIGHT, ESCAPE]
+	if user.get_room_temp(ACTIVATED, True):
+		return user.get_fight_actions()
+
+	else:
+		if user.has_tag('nazgul_killer') and user.has_tag('shire_burned'):
+
+			if user.has_item_tag('ring'):
+
+				if user.has_tag('met_gollum'):
+					return actions_state_0
+
+				else:
+					return actions_state_2
+
+			else:
+				return actions_state_1
+
+		else:
+			return actions_state_0
+
+
+def dice(user, reply, result, subject=None):
+	if result > DICE_MIDDLE:
+		reply(
+			'Тебе удалось!'
+			'Надеюсь мы больше не встретим этого мерзкого типа!'
+		)
+		user.leave(reply)
+
+	else:
+		reply(
+			'Не вышло!\n'
+			'Ну ни чего я буду за тебя болеть!'
+		)
+		user.set_room_temp(ACTIVATED, True)
+
+
+def enter(user, reply):
+	if user.has_tag('nazgul_killer') and user.has_tag('shire_burned'):
+
+		if user.has_item_tag('rigs'):
+
+			if user.has_tag('met_gollum'):
+
+				reply('Темный Лорд исчез')
+
+			else:
+
+				hp = 3000
+
+				user.remove_item('rigs')  # У игрока должно быть только одно кольцо!
+				reply(
+					'«_Я чувстую его! Оно мое!_»\n\n'
+					'Мамочки! Мне страшно!\n'
+					'Верни ему то что он просит! НЕМЕДЛЕННО!'
+				)
+
+		else:
+			reply(
+				'Темный Лорд медленно поворачивается и замечает незваного гостя\n\n'
+				'Мамочки! что сейчас будет!!!'
+			)
+
+	else:
+		reply(
+			'Кажется мы оказались не в том месте в не то время.\n'
+			'Очень надеюсь что {} не обратит на нас внимание.'.format(name)
+		)
+
+		user.set_room_temp(ACTIVATED, False)
+
+
+def action(user, reply, text):
+	if user.get_room_temp(ACTIVATED, True):
+		user.fight_action(reply, text)
+
+	else:
+
+		if user.has_tag('nazgul_killer') and user.has_tag('shire_burned'):
+
+			if user.has_item_tag('rigs'):
+
+				if user.has_tag('met_gollum'):
+
+					if text == actions_state_0[0]:
+						user.leave(reply)
+
+			else:
+				# Кольцо
+				if text == actions_state_2[0]:
+					user.set_room_temp(ACTIVATED, True)
+
+		else:
+			# Назгулы
+			if text == actions_state_1[0]:
+				user.set_room_temp(ACTIVATED, True)
+
+			else:
+				reply('Кидай кубики')
+				user.throw_dice(reply)
+
+	user.leave(reply)
