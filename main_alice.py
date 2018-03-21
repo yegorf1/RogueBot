e1e4373b415ccc765f92c9e21b3b0111fedfa09b
Arseniy <molodeckij.b.2017@fml31.ru>
2018-03-21 23:50:42 +0300

    added main_alice.py file

---
+++ main_alice.py
@@ -0,0 +1,65 @@
+# coding utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+import usermanager
+import config
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+        res['response']['text'] = 'Теперь скажи мне свое имя'
+        return
+    text =''
+    usermanager.message(uid, lambda *a, **kw: text = text + reply(c_id, *a, **kw) + '\n', event[6])
+    # Пользователь согласился, прощаемся.
+    res['response']['text'] = text
+    return
+
+
+def reply(id, txt, buttons=None, photo=None):
+    return txt
+
+
+
